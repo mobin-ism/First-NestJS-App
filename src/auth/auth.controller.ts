44a5ed72d58dac83fc8000c413c1571e95d4711a
Al Mobin <instance.of.venture@gmail.com>
2022-04-18 03:40:55 +0600

    Done the authorization with JWT Passport.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { LocalAuthGurd } from './local-auth.guard';
+import { AuthService } from './auth.service';
+import { JwtAuthGurd } from './jwt-auth.guard';
 
 @Controller()
 export class AuthController {
 
+    constructor(private authservice : AuthService) {}
     @UseGuards(LocalAuthGurd)
     @Post('login')
     login(@Request() req) : any {
-        return req.user;
+        return this.authservice.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGurd)
+    @Get('/dashboard')
+    dashboard(@Request() req) : any {
+        return `Welcome to ${req.user.name}'s Dashboard `;
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalSrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
-
+import { JwtModule } from '@nestjs/jwt';
+import { JwtSrategy } from './jwt.strategy';
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalSrategy],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    secret : 'SECRET_KEY',
+    signOptions : {expiresIn: '60s'},
+  })],
+  providers: [AuthService, LocalSrategy, JwtSrategy],
   controllers: [AuthController]
 })
 
